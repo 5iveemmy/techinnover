44d5377e567ce29ba215de24e94d88ecbbf510d4
5iveemmy <ainemmanuel87@gmail.com>
2022-04-06 12:58:40 +0100

    change react version

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,14 @@
-import React from "react";
 import styled from "styled-components";
+import React from "react";
 
-const Nav = styled.div``;
+const Nav = styled.nav``;
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <h1>whatsup</h1>
+    </Nav>
+  );
+};
+
+export default Navbar;
